76ee549f4fa86e231ea8f709eae19c8967eb7cb9
Sparklegift <godsgiftetuk2001@gmail.com>
2025-07-16 18:25:22 +0100

    pull

--- src/app/cpanel/page.tsx
+++ src/app/cpanel/page.tsx
@@ -1,5 +1,5 @@
 import LoginForm from "@/components/admin/login-form"
-import Link from "next/link"
+
 
 export default function CpanelPage() {
   return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/no-unescaped-entities */
 import Link from "next/link"
 import Image from "next/image"
 import { Merriweather, DM_Sans, Great_Vibes } from 'next/font/google';
